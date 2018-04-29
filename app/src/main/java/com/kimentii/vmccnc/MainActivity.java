14fc03ec09fa9be3ff090b4959c1929f12be45d5
Kimentii <malafeikirill@mail.ru>
2018-04-29 21:28:35 +0300

    initial commit.

---
+++ app/src/main/java/com/kimentii/vmccnc/MainActivity.java
@@ -0,0 +1,35 @@
+package com.kimentii.vmccnc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewPager = findViewById(R.id.vp_machines);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+        });
+    }
+}
