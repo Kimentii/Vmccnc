36b4de1e8eca2d8d68a67acc9d79b9fef54bf56c
Kimentii <malafeikirill@mail.ru>
2018-04-30 11:39:21 +0300

    Added navigation view.

--- app/src/main/java/com/kimentii/vmccnc/MainActivity.java
+++ app/src/main/java/com/kimentii/vmccnc/MainActivity.java
@@ -1,12 +1,18 @@
 package com.kimentii.vmccnc;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mViewPager = findViewById(R.id.vp_machines);
         final FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
@@ -38,5 +46,40 @@ public class MainActivity extends AppCompatActivity {
         });
         TabLayout tabLayout = findViewById(R.id.tl_dots);
         tabLayout.setupWithViewPager(mViewPager, true);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        setupNavigationViewOnItemSelectedListener(navigationView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_more:
+                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(Gravity.LEFT);
+                break;
+        }
+        return true;
+    }
+
+    private void setupNavigationViewOnItemSelectedListener(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        handleNavigationViewItemSelected(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void handleNavigationViewItemSelected(MenuItem menuItem) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawers();
     }
 }
